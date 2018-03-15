5ccb3d77eba58788decbc9f91e84b1116136ebed
Bence Tureczki <tureczkibence@gmail.com>
2018-03-15 17:22:11 +0100

    Writing POM documentation.

--- projects/projectmico/src/main/java/org/projectmico/artifact/Application.java
+++
@@ -1,8 +0,0 @@
-package org.projectmico.artifact;
-
-public class Application {
-   public static void main( String[] args ){
-	  String hellomico = "Hi! I am mICO! ^_^";
-      System.out.println( hellomico );
-   }
-}
