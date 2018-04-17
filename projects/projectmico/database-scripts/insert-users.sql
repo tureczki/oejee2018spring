28356159c98e78607ccf75e3de52395a4fc07842
Bence Tureczki <tureczkibence@gmail.com>
2018-04-17 18:02:40 +0200

    New DB script: Insert data: Users

---
+++ projects/projectmico/database-scripts/insert-users.sql
@@ -0,0 +1,4 @@
+INSERT INTO Users VALUES
+    ('investor.tureczki@greattureczkibank.eu', '99777JDZEWF', 'Investor', 'Investment', 'Dark', 'USD');
+INSERT INTO Users VALUES
+    ('programmer.tureczki@greattureczkibank.eu', '28374JDIEOR', 'Programmer', 'Cooperation', 'Light', 'EUR');
