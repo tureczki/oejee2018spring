c88630cec7330da79d85f3d7b87fa2ed41f7d64c
Bence Tureczki <tureczkibence@gmail.com>
2018-04-25 22:50:26 +0200

    New remote interface: Remote search: UserSearch

--- projects/projectmico/pm-ejbservice/src/main/java/org/tureczki/projectmico/ejbservice/facade/UserFacadeImpl.java
+++ projects/projectmico/pm-ejbservice/src/main/java/org/tureczki/projectmico/ejbservice/facade/UserFacadeImpl.java
@@ -3,11 +3,14 @@ package org.tureczki.persistence.service;
 import org.tureczki.persistence.service.UserService;
 import org.tureczki.projectmico.ejbservice.converter.UserConverter;
 import org.tureczki.projectmico.ejbservice.domain.UserStub;
+import org.tureczki.projectmico.ejbserviceclient.domain.UserStubRemote;
 import org.tureczki.projectmico.ejbservice.facade.UserFacade;
+import org.tureczki.projectmico.ejbserviceclient.UserFacadeRemote;
 import javax.ejb.*;
 import org.apache.log4j.Logger;
+import javax.xml.rpc.ServiceException;
 
-@Stateless(mappedName = "ejb/userFacade", name = "UserFacadeRemote.UserService")
+@Stateless(mappedName = "ejb/userFacade", name = UserFacadeRemote.BEAN_NAME)
 public class UserFacadeImpl implements UserFacade, UserFacadeRemote {
 
  private static final Logger LOGGER = Logger.getLogger(UserFacadeImpl.class);
@@ -17,11 +20,26 @@ public class UserFacadeImpl implements UserFacade, UserFacadeRemote {
 
  @EJB
  private UserConverter converter;
-
+ 
  @Override
  public UserStub getUserStub(String email) throws Exception {
   try {
    final UserStub stub = this.converter.to(this.service.readUser(email));
+   if (LOGGER.isDebugEnabled()) {
+    LOGGER.debug("Get user by email (" + email + ") --> " + stub);
+   }
+   return stub;
+  } catch (final Exception e) {
+   LOGGER.error(e, e);
+   throw new Exception(e.getLocalizedMessage());
+  }
+ }
+ 
+ @Override
+ public UserStubRemote getUserStubRemote(String email) throws Exception {
+  try {
+   final UserStub user = this.converter.to(this.service.readUser(email));
+   UserStubRemote stub = new UserStubRemote(user.getEmail(), user.getPasswordhash(), user.getExperience(), user.getSubscriber(), user.getTheme(), user.getCurrency());
    if (LOGGER.isDebugEnabled()) {
     LOGGER.debug("Get user by email (" + email + ") --> " + stub);
    }
--- projects/projectmico/pm-ejbserviceclient/src/main/java/org/tureczki/projectmico/ejbserviceclient/UserFacadeRemote.java
+++ projects/projectmico/pm-ejbserviceclient/src/main/java/org/tureczki/projectmico/ejbserviceclient/UserFacadeRemote.java
@@ -1,12 +1,12 @@
 package org.tureczki.projectmico.ejbserviceclient;
 
 import javax.ejb.Remote;
-import org.tureczki.projectmico.ejbserviceclient.domain.UserStub;
+import org.tureczki.projectmico.ejbserviceclient.domain.UserStubRemote;
 
 @Remote
 public interface UserFacadeRemote{
 	
 	String BEAN_NAME = "UserService";
-	UserStub getUserStub (String email) throws ServiceExceptions;
+	UserStubRemote getUserStubRemote (String email) throws Exception;
 	
 }
